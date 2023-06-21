76f1f0309e40707f4ec1b9ffe78d65f221027b60
Analu Dourado <anabluoliveira@outlook.com>
2023-06-21 09:31:36 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components"; 
+
+export const Container = styled.div` height: 150px; text-align: center; background: #441464; `; 
+
+export const Header = styled.h1``; 
+
+export const Title = styled.div` padding-top: 20px; color: #fff; `;
