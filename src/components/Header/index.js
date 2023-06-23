7383d6a724eaac0029e3f5120e116b12520ea7fd
Ana <ana.dourado@lab2dev.com>
2023-06-22 22:44:25 -0300

    Title

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ export const Header = () => {
     return ( 
     <C.Container> 
         <C.Header> 
-            <C.Title> Controle Financeiro </C.Title> 
+            <C.Title> MicroGestor </C.Title> 
         </C.Header> 
     </C.Container> );
 }; 
